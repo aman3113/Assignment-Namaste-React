d114d55f54abb7195667e43c23184bc18f8f57da
AMAN <antilaman3113@gmail.com>
2022-12-29 17:25:44 +0530

    add: added class components and restructured folders

---
+++ assignment5-class/src/routes/app.js
@@ -0,0 +1,38 @@
+import Body from "../Components/bodyComponent/bodyComponent.js";
+import SearchBar from "../Components/search-bar/searchBar.js";
+import PageNotFound from "./AppLayout/PageNotFound";
+import "./app.css";
+import { getDevelopers } from "../Data/SharedFunctions.js";
+import { useState, useEffect } from "react";
+
+export default App = () => {
+  const [userData, setUserData] = useState([]);
+  //
+  const [filteredData, setFilteredData] = useState([]);
+  //
+  const [matchFound, setMatchFound] = useState(true);
+  useEffect(() => {
+    getDevelopers();
+    setUserData(JSON.parse(sessionStorage.getItem("developerArray")));
+  }, []);
+
+  return (
+    <>
+      <SearchBar
+        userData={userData}
+        setFilteredData={setFilteredData}
+        setMatchFound={setMatchFound}
+      />
+
+      {matchFound ? (
+        filteredData.length > 0 ? (
+          <Body filteredData={filteredData} />
+        ) : (
+          <Body filteredData={userData} />
+        )
+      ) : (
+        <PageNotFound />
+      )}
+    </>
+  );
+};
